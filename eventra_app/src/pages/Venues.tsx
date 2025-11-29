d0d3a65282f59e69d8b2902cca90069542e86d1e
HumbalH <humbalgr8@gmail.com>
2025-11-29 10:40:57 +0500

    Fixed the icon and text overlap in input fields

--- eventra_app/src/pages/Venues.tsx
+++ eventra_app/src/pages/Venues.tsx
@@ -80,7 +80,7 @@ const Venues: React.FC = () => {
             placeholder="Search venues..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="input-field pl-10"
+            className="block w-full pl-10 pr-10 py-2 rounded-md border border-gray-300"
           />
         </div>
       </div>
