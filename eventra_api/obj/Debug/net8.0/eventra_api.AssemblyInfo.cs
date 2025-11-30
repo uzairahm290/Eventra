a1ae152a25140d8195a6a48c6051492123422456
auonnaseer23 <auonnaseer23@gmail.com>
2025-11-30 13:36:25 +0500

    EV-API: Added SearchController.cs

--- eventra_api/obj/Debug/net8.0/eventra_api.AssemblyInfo.cs
+++ eventra_api/obj/Debug/net8.0/eventra_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eventra_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f14b9dbe536ad38e17adec4bf898ce68aa0fbd51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+530dd2a3213c83791a1a50adecef00ebe2147a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("eventra_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eventra_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
