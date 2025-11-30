3674d75a50f3fc8d6d22580c28efe36d798a997d
Uzair Ahmad <uzairahm290@gmail.com>
2025-11-30 18:46:51 +0500

    EV: Remove Docekr File

--- ViewTableData.sql
+++ ViewTableData.sql
@@ -3,8 +3,6 @@ SELECT * FROM AspNetUsers;
 SELECT * FROM Venues;
 SELECT * FROM Events;
 SELECT * FROM Menus;
-SELECT * FROM EventAttendees;
 SELECT * FROM Bookings;
-SELECT * FROM Notifications;
 SELECT * FROM AuditLogs;
 SELECT * FROM RefreshTokens;
